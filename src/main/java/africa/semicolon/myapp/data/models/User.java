ce6e3a86ef55b63fac37a2a1ac02e4013324622f
dark-royal <praiseoyewole560@gmail.com>
2024-08-28 15:08:01 +0100

    changed the url

--- src/main/java/africa/semicolon/myapp/data/models/User.java
+++ src/main/java/africa/semicolon/myapp/data/models/User.java
@@ -1,15 +1,13 @@
 package africa.semicolon.myapp.data.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Entity
+@Table(name = "app-user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
